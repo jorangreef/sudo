62b3d18e68da6b256e17f6a67cda6bb0aacffb97
Jose-Luis Rivas <me@ghostbar.co>
2015-09-06 13:53:26 -0500

    test: sudo -k so it always shows a prompt

--- test.js
+++ test.js
@@ -1,9 +1,15 @@
 var sudo = require('./');
 var test = require('tape');
+var exec = require('child_process').exec;
+
+function cleanSudo () {
+  exec('sudo -k');
+}
 
 test('a simple call works', function (t) {
   sudo.exec('echo \'name\'', function (err) {
     t.error(err, 'no error');
+    cleanSudo();
     t.end();
   });
 });
@@ -12,6 +18,7 @@ test('try setting a name', function (t) {
   sudo.setName('this is a test');
   sudo.exec('echo \'name\'', function (err) {
     t.error(err, 'no error');
+    cleanSudo();
     t.end();
   })
 })
