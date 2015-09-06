6d4c279de48c2a36fb6ba8611d99ce7cdac70933
Jose-Luis Rivas <me@ghostbar.co>
2015-09-06 13:37:52 -0500

    test setting a name

--- test.js
+++ test.js
@@ -3,6 +3,15 @@ var test = require('tape');
 
 test('a simple call works', function (t) {
   sudo.exec('echo \'name\'', function (err) {
-    t.end(err);
+    t.error(err, 'no error');
+    t.end();
   });
 });
+
+test('try setting a name', function (t) {
+  sudo.setName('this is a test');
+  sudo.exec('echo \'name\'', function (err) {
+    t.error(err, 'no error');
+    t.end();
+  })
+})
