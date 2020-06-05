e579b97651894d9e354613606a55c92f1bf6c11b
Vladyslav Dalechyn <hotwater438@tutanota.com>
2020-06-05 12:41:02 +0300

    Provide namespace for promisyfing
    Allows having async/await calls as child_process.exec does.

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,24 @@ export function exec(cmd: string,
         options?: ((error?: Error, stdout?: string | Buffer, stderr?: string | Buffer) => void)
                 | { name?: string, icns?: string, env?: { [key: string]: string } },
         callback?: (error?: Error, stdout?: string | Buffer, stderr?: string | Buffer) => void): void;
+
+// NOTE: This namespace provides design-time support for util.promisify. Exported members do not exist at runtime.
+namespace exec {
+  function __promisify__(command: string): PromiseWithChild<{ stdout: string; stderr: string }>
+  function __promisify__(
+    command: string,
+    options: { encoding: 'buffer' | null } & ExecOptions
+  ): PromiseWithChild<{ stdout: Buffer; stderr: Buffer }>
+  function __promisify__(
+    command: string,
+    options: { encoding: BufferEncoding } & ExecOptions
+  ): PromiseWithChild<{ stdout: string; stderr: string }>
+  function __promisify__(
+    command: string,
+    options: ExecOptions
+  ): PromiseWithChild<{ stdout: string; stderr: string }>
+  function __promisify__(
+    command: string,
+    options?: ({ encoding?: string | null } & ExecOptions) | null
+  ): PromiseWithChild<{ stdout: string | Buffer; stderr: string | Buffer }>
+}
